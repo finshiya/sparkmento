a5f4913deccc8f7ad417bbfffaa1304fa6dd719a
finshiya <finshiyavp0102@gmail.com>
2025-11-15 15:49:50 +0530

    loader effect added

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,12 @@
         video.muted = true;
       }
     });
+
+    // loader
+      window.addEventListener("load", function () {
+    const loader = document.getElementById("page-loader");
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 700);
+  });
